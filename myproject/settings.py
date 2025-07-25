0aef33c4caff2c8e63c6b8195563bf2e2ab3e05a
reme25241-cloud <reme25241@gmail.com>
2025-07-25 06:48:23 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = ''  # Replace with your email password or app-specific password
+EMAIL_HOST_USER = 'reme25241@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'kqyj ciyn dljr ggyz'  # Replace with your email password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
